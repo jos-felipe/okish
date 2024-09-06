ae04d96ac3c749825934b2d2ec0d44b1cf97318f
josfelip@debian <josfelip@debian>
2024-09-06 15:03:18 -0300

    functional

--- main.c
+++ main.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 17:09:14 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/09/05 14:03:11 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/09/06 14:55:54 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/minishell.h"
 
+static char	*mini_pwd_prompt(t_mini *mini);
 static void	mini_no_error_detect(t_mini *mini);
 static void	mini_recursive_init(t_mini *mini);
 
@@ -50,63 +51,39 @@ void	mini_no_error_detect(t_mini *mini)
 	mini_execute(mini);
 }
 
-#include <string.h>
- 
-#define PROMPT_LEN 11
-
 void	mini_recursive_init(t_mini *mini)
 {
-	char	msg[PROMPT_LEN + 1];
-	char	*str;
 	char	*prompt;
-	char	**split_pwd = NULL;
-	int		i;
 	
 	mini_init(mini);
-	prompt = mini_get_prompt_msg(mini);
 	mini->int_action.sa_handler = sig_handler;
 	sigaction(SIGINT, &mini->int_action, NULL);
 	mini->quit_action.sa_handler = SIG_IGN;
 	sigaction(SIGQUIT, &mini->quit_action, NULL);
-	// mini->cmd_line = readline("prompt > ");
+	prompt = mini_pwd_prompt(mini);
 	mini->cmd_line = readline(prompt);
+	free(prompt);
 	ft_collect_mem(mini->cmd_line);
 }
 
-char	*mini_get_prompt_msg(t_mini *mini)
+char	*mini_pwd_prompt(t_mini *mini)
 {
+	char	*prompt;
 	char	*pwd;
-	char	**pwd_split;
-	int		len;
+	char	*tmp;
 
-	len = 0;
-	pwd_split = NULL;
+	prompt = ft_calloc(PROMPT_LEN + 1, sizeof(char));
 	pwd = ft_dict_get_value(mini->env_list, "PWD");
-	if (pwd != NULL)
-	{
-		len = strlen(pwd);
-		pwd_split = ft_split(pwd, '/');
-		if (pwd_split != NULL)
-			pwd = mini_get_last_split(pwd_split);
-	}
-	ft_memcpy(msg, last_dir, PROMPT_LEN - 2);
-	prompt = ft_strjoin(last_dir, "/$ ");
-	return (msg);
+	if (pwd == NULL)
+		return(ft_strdup("?/$ "));
+	tmp = ft_split_get_last(pwd, '/');
+	if (tmp == NULL)
+		return(ft_strdup("?/$ "));
+	pwd = ft_strjoin(tmp, "/$ ");
+	ft_memcpy(prompt, pwd, ft_min(strlen(pwd), PROMPT_LEN));
+	free(pwd);
+	free(tmp);
+	return (prompt);
 }
 
-char	*mini_get_last_split(char *str, char sep)
-{
-	char	*last_str;
-	char	**str_split;
-	int		i;
-	
-	last_str = NULL;
-	i = 0;
-	str_split = ft_split(str, sep);
-	if (str_split != NULL)
-	{
-		while (str_split[i])
-			last_str = str_split[i++];
-	}
-	return (last_str);
-}
+
--- src/ft_substr_mem.c
+++ src/ft_substr_mem.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr_mem.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 13:01:36 by josfelip          #+#    #+#             */
-/*   Updated: 2024/06/17 12:27:24 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/09/06 14:29:02 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,47 @@ char	*ft_substr_mem(char *str, int start, int len)
 	sub = ft_substr(str, start, len);
 	ft_collect_mem(sub);
 	return (sub);
+}
+
+void	ft_split_free(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+char	*ft_split_get_last(char *str, char sep)
+{
+	char	*last;
+	char	**split;
+	int		i;
+	
+	last = NULL;
+	i = 0;
+	split = ft_split(str, sep);
+	if (split != NULL)
+	{
+		while (split[i])
+			last = split[i++];
+	}
+	if (last != NULL)
+		last = ft_strdup(last);
+	ft_split_free(split);
+	return (last);
+}
+
+size_t	ft_min(size_t a, size_t b)
+{
+	size_t	min;
+
+	min = a;
+	if (min > b)
+		min = b;
+	return (min);
 }
